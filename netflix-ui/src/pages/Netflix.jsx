ae55aed08f5046f1aa1333e05a8ba9ad2cb55002
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-20 13:54:08 +0100

    Build: The netflix home page

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import backgroungImage from "../assets/home.jpg";
+import MovieLogo from "../assets/homeTitle.webp";
+import { FaPlay } from "react-icons/fa";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import styled from "styled-components";
 
 export default function Netflix() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div>Netflix</div>
-  )
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="hero">
+        <img
+          src={backgroungImage}
+          alt="background"
+          className="background-image"
+        />
+        <div className="container">
+          <div className="logo">
+            <img src={MovieLogo} alt="Movie Logo" />
+          </div>
+
+          <div className="button flex">
+            <button className="flex j-center a-center">
+              <FaPlay /> Play
+            </button>
+            <button className="flex j-center a-center">
+              <AiOutlineInfoCircle /> More Info
+            </button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  background-color: black;
+  .hero {
+    position: relative;
+    .background-image {
+      fliter: brightness(60%);
+    }
+    img {
+      height: 100vh;
+      width: 100vw;
+    }
+    .container {
+      position: absolute;
+      bottom: 5rem;
+      .logo {
+        img {
+          width: 100%;
+          height: 100%;
+          marging-left: 5rem;
+        }
+      }
+      .button {
+        margin: 5rem;
+        gap: 2rem;
+        button {
+          font-size: 1.4rem;
+          gap: 1rem;
+          border-radius: 0.2rem;
+          padding: 0.5rem;
+          padding-left: 2rem;
+          padding-right: 2.4rem;
+          border: none;
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          &:hover {
+            opacity: 0.8;
+          }
+          &:nth-of-type(2) {
+            background-color: rgba(109, 109, 110, 0.7);
+            color: white;
+            svg {
+              font-size: 1.8rem;
+            }
+          }
+        }
+      }
+    }
+  }
+`;
