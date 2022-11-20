04a81c341caba90e1ac3af11af3edcac55b20b1f
Maulik Pansuriya <maulikpansuriya36@gmail.com>
2022-11-20 09:45:02 +0100

    Build: Signup page

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from "./pages/Login.jsx"
+import Signup from "./pages/Signup.jsx"
+import Netflix from "./pages/Netflix.jsx"
 
 export default function App() {
   return (
-    <BrowserRouter></BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path='/login' element={<Login />} />
+        <Route exact path='/signup' element={<Signup />} />
+        <Route exact path='/' element={<Netflix />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,7 +1,45 @@
 import React from 'react'
+import styled from "styled-components";
+import BackgroundImage from '../components/BackgroundImage'
+import Header from '../components/Header'
 
 export default function signup() {
-  return (
-    <div>signup</div>
-  )
+  return <Container>
+    <BackgroundImage />
+
+    <div className="content">
+    <Header />
+    <div className="body flex column a-center j-center">
+        <div className="text flex column">
+          <h1>Unlimited movies, TV Shows and more</h1>
+          <h4>watch anywhere. Cancel anytime.</h4>
+          <h6>
+            Ready to watch? Enter your email to create or restart membership
+          </h6>
+        </div>
+
+        <div className="form">
+          <input type="email" placeholder='Enter you email here...' name='email' />
+          <input type="password" name="enter your password here..." id="password" />
+          <button>Get Started</button>
+        </div>
+
+        <button>Log In</button>
+     </div>
+    </div>
+  </Container>
 }
+
+const Container = styled.div`
+  position: relative;
+  .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0 ,0 ,0 ,0.5);
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rowa: 15vh 85vh;
+  } 
+`;
